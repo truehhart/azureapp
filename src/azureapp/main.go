5f2387cc1e7c12e21ac5b43b5f67ed649e437c13
Dmitry Parshenkov <pd@mightycall.com>
2023-01-29 01:35:02 +0500

    add test

--- src/azureapp/main.go
+++ src/azureapp/main.go
@@ -11,20 +11,20 @@ import (
 
 func main() {
 	// Quick check for necessary environment variables.
-	reqEnvironmentVars := []string{
-		"HTTP_LISTEN_PORT",
-		"APP_MESSAGE",
-		"APP_ENVIRONMENT",
-		"APP_REVISION",
-	}
-	for _, variable := range reqEnvironmentVars {
-		_, present := os.LookupEnv(variable)
-		if !(present) {
-			log.Fatalf("%s environment variable is not defined. The following environment variables must be defined: %v", variable, reqEnvironmentVars)
-		}
-	}
+	// reqEnvironmentVars := []string{
+	// 	"HTTP_LISTEN_PORT",
+	// 	"APP_MESSAGE",
+	// 	"APP_ENVIRONMENT",
+	// 	"APP_REVISION",
+	// }
+	// for _, variable := range reqEnvironmentVars {
+	// 	_, present := os.LookupEnv(variable)
+	// 	if !(present) {
+	// 		log.Fatalf("%s environment variable is not defined. The following environment variables must be defined: %v", variable, reqEnvironmentVars)
+	// 	}
+	// }
 
 	listenPort, _ := os.LookupEnv("HTTP_LISTEN_PORT")
 	
-	http.ListenAndServe(strings.Join([]string{":", listenPort}, ""), routes.Router())
+	http.ListenAndServe(strings.Join([]string{":", "8080"}, ""), routes.Router())
 }
